952baa473ac454884159633b570ddce3a852e13a
hiroya iizuka <hiroyaiizuka@hiroyanoMacBook-Pro.local>
2018-12-20 20:49:25 +0900

    Updates

---
+++ precache-manifest.b7f02e6f531f5257a1ac978f8dcc074b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "306dc190e6a39fb013ad",
+    "url": "/SPA_AntibioticSoon/static/js/runtime~main.306dc190.js"
+  },
+  {
+    "revision": "85c60e67ed75baf699c0",
+    "url": "/SPA_AntibioticSoon/static/js/main.85c60e67.chunk.js"
+  },
+  {
+    "revision": "fcff6e1493a310e17505",
+    "url": "/SPA_AntibioticSoon/static/js/1.fcff6e14.chunk.js"
+  },
+  {
+    "revision": "85c60e67ed75baf699c0",
+    "url": "/SPA_AntibioticSoon/static/css/main.6fd32d8e.chunk.css"
+  },
+  {
+    "revision": "b463db75d148940812821475cec170e9",
+    "url": "/SPA_AntibioticSoon/index.html"
+  }
+];
